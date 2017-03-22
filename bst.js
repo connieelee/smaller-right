66f71cedfa4e3d626c2482c32ffd3204c7138bf2
Connie Lee <connie.lee@fullstackacademy.com>
2017-03-22 09:52:46 -0500

    update variable naming

--- bst.js
+++ bst.js
@@ -6,17 +6,17 @@ function BST(value) {
 }
 
 BST.prototype.insert = function(value) {
-  let leftCount = 0;
+  let nSmaller = 0;
   this.size++;
   if (value <= this.value) {
     if (!this.left) this.left = new BST(value);
-    else leftCount += this.left.insert(value);
+    else nSmaller += this.left.insert(value);
   } else {
-    leftCount += (this.left ? this.left.size : 0) + 1;
+    nSmaller += (this.left ? this.left.size : 0) + 1;
     if (!this.right) this.right = new BST(value);
-    else leftCount += this.right.insert(value);
+    else nSmaller += this.right.insert(value);
   }
-  return leftCount;
+  return nSmaller;
 }
 
 module.exports = BST;
